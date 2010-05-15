ea8daaf801e2d3df11aacb0ff2eb33b22a44a16d
Florian Ragwitz <rafl@debian.org>
2010-05-15 18:59:06 +0200

    Initial version

---
+++ lib/MooseX/Types/Meta.pm
@@ -0,0 +1,49 @@
+package MooseX::Types::Meta;
+
+use MooseX::Types -declare => [qw(
+    TypeConstraint
+    TypeCoercion
+    Attribute
+    Method
+    Class
+    Role
+
+    TypeEquals
+    TypeOf
+    SubtypeOf
+
+    StructuredTypeConstraint
+    StructuredTypeCoercion
+
+    ParameterizableRole
+    ParameterizedRole
+
+)];
+use namespace::clean;
+
+class_type TypeConstraint, { class => 'Moose::Meta::TypeConstraint' };
+class_type TypeCoercion,   { class => 'Moose::Meta::TypeCoercion'   };
+class_type Attribute,      { class => 'Class::MOP::Attribute'       };
+class_type Method,         { class => 'Class::MOP::Method'          };
+class_type Class,          { class => 'Class::MOP::Class'           };
+class_type Role,           { class => 'Moose::Meta::Role'           };
+
+class_type StructuredTypeConstraint, {
+    class => 'MooseX::Meta::TypeConstraint::Structured',
+};
+
+class_type StructuredTypeCoercion, {
+    class => 'MooseX::Meta::TypeCoercion::Structured',
+};
+
+role_type ParameterizableRole, {
+    role => 'MooseX::Role::Parameterized::Meta::Role::Parameterizable',
+};
+
+role_type ParameterizedRole, {
+    role => 'MooseX::Role::Parameterized::Meta::Role::Parameterized',
+};
+
+__PACKAGE__->meta->make_immutable;
+
+1;
