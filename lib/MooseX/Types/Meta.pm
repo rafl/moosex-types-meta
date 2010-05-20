d032fc4986106190676595f78fca8bd4d8651e86
Florian Ragwitz <rafl@debian.org>
2010-05-20 23:02:21 +0200

    Add some docs

--- lib/MooseX/Types/Meta.pm
+++ lib/MooseX/Types/Meta.pm
@@ -28,29 +28,118 @@ use namespace::clean;
 # TODO: ParameterizedType{Constraint,Coercion} ?
 #       {Duck,Class,Enum,Parameterizable,Parameterized,Role,Union}TypeConstraint?
 
-class_type TypeConstraint, { class => 'Moose::Meta::TypeConstraint'  };
-class_type TypeCoercion,   { class => 'Moose::Meta::TypeCoercion'    };
-class_type Attribute,      { class => 'Class::MOP::Attribute'        };
+=type TypeConstraint
+
+A L<Moose::Meta::TypeConstraint>.
+
+=cut
+
+class_type TypeConstraint, { class => 'Moose::Meta::TypeConstraint' };
+
+=type TypeCoercion
+
+A L<Moose::Meta::TypeCoercion>.
+
+=cut
+
+class_type TypeCoercion,   { class => 'Moose::Meta::TypeCoercion' };
+
+=type Attribute
+
+A L<Class::MOP::Attribute>.
+
+=cut
+
+class_type Attribute,      { class => 'Class::MOP::Attribute' };
+
+=type RoleAttribute
+
+A L<Moose::Meta::Role::Attribute>.
+
+=cut
+
 class_type RoleAttribute,  { class => 'Moose::Meta::Role::Attribute' };
-class_type Method,         { class => 'Class::MOP::Method'           };
-class_type Class,          { class => 'Class::MOP::Class'            };
-class_type Role,           { class => 'Moose::Meta::Role'            };
+
+=type Method
+
+A L<Class::MOP::Method>.
+
+=cut
+
+class_type Method,         { class => 'Class::MOP::Method' };
+
+=type Class
+
+A L<Class::MOP::Class>.
+
+=cut
+
+class_type Class,          { class => 'Class::MOP::Class' };
+
+=type Role
+
+A L<Moose::Meta::Role>.
+
+=cut
+
+class_type Role,           { class => 'Moose::Meta::Role' };
+
+=type StructuredTypeConstraint
+
+A L<MooseX::Meta::TypeConstraint::Structured>.
+
+=cut
 
 class_type StructuredTypeConstraint, {
     class => 'MooseX::Meta::TypeConstraint::Structured',
 };
 
+=type StructuredTypeCoercion
+
+A L<MooseX::Meta::TypeCoercion::Structured>.
+
+=cut
+
 class_type StructuredTypeCoercion, {
     class => 'MooseX::Meta::TypeCoercion::Structured',
 };
+
+=type ParameterizableRole
+
+A L<MooseX::Role::Parameterized::Meta::Role::Parameterizable>.
+
+=cut
 
 class_type ParameterizableRole, {
     class => 'MooseX::Role::Parameterized::Meta::Role::Parameterizable',
 };
 
+=type ParameterizedRole
+
+A L<MooseX::Role::Parameterized::Meta::Role::Parameterized>.
+
+=cut
+
 class_type ParameterizedRole, {
     class => 'MooseX::Role::Parameterized::Meta::Role::Parameterized',
 };
+
+=type TypeEquals[`x]
+
+A L<Moose::Meta::TypeConstraint>, that's equal to the type constraint
+C<x>.
+
+=type TypeOf[`x]
+
+A L<Moose::Meta::TypeConstraint>, that's either equal to or a subtype
+of the type constraint C<x>.
+
+=type SubtypeOf[`x]
+
+A L<Moose::Meta::TypeConstraint>, that's a subtype of the type
+constraint C<x>.
+
+=cut
 
 for my $t (
     [ 'TypeEquals', 'equals'        ],
